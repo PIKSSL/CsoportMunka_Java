b12488f4f2c66be5ee067a298b3fb19aba2ed022
PIKSSL <purpledog120@gmail.com>
2022-03-07 13:24:40 +0100

    alap

---
+++ src/csoportmunka_java/CsoportMunka_Java.java
@@ -0,0 +1,43 @@
+package csoportmunka_java;
+public class CsoportMunka_Java {
+
+    public static void main(String[] args) {
+        //Bekérés metódusban  || menü
+        menu();
+    }
+
+    private static void menu() { //Menü
+        
+        //Négy fő megtódus 
+        osszeadas();
+        kivonas();
+        szorzas();
+        osztas();
+        
+    }
+    static void bekeres(String szoveg){ 
+        
+    }
+    static int ellenorzo(String adat){ //Szöveg/szám ellenőrzés
+        
+        int vegleges = 0; //érték csere
+        return vegleges;  
+    }
+
+    static void osszeadas() {
+        
+    }
+
+    static void kivonas() {
+        
+    }
+
+    static void szorzas() {
+        
+    }
+
+    static void osztas() {
+        
+    }
+    
+}
